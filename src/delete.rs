eaafc54f51e7801878a4fe12233d5a8069f62068
Kenny Flegal <soulstompp@gmail.com>
2022-08-20 11:24:20 -0700

    parse table partitions for deletion

--- src/delete.rs
+++ src/delete.rs
@@ -1,26 +1,25 @@
 use nom::character::complete::multispace1;
 use std::{fmt, str};
 
-use common::{schema_table_reference, statement_terminator};
+use common::{statement_terminator, table_object};
 use condition::ConditionExpression;
-use keywords::escape_if_keyword;
 use nom::bytes::complete::tag_no_case;
 use nom::combinator::opt;
 use nom::sequence::{delimited, tuple};
 use nom::IResult;
 use select::where_clause;
-use table::Table;
+use table::TableObject;
 
 #[derive(Clone, Debug, Default, Eq, Hash, PartialEq, Serialize, Deserialize)]
 pub struct DeleteStatement {
-    pub table: Table,
+    pub table: TableObject,
     pub where_clause: Option<ConditionExpression>,
 }
 
 impl fmt::Display for DeleteStatement {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "DELETE FROM ")?;
-        write!(f, "{}", escape_if_keyword(&self.table.name))?;
+        write!(f, "{}", self.table)?;
         if let Some(ref where_clause) = self.where_clause {
             write!(f, " WHERE ")?;
             write!(f, "{}", where_clause)?;
@@ -33,7 +32,7 @@ pub fn deletion(i: &[u8]) -> IResult<&[u8], DeleteStatement> {
     let (remaining_input, (_, _, table, where_clause, _)) = tuple((
         tag_no_case("delete"),
         delimited(multispace1, tag_no_case("from"), multispace1),
-        schema_table_reference,
+        table_object,
         opt(where_clause),
         statement_terminator,
     ))(i)?;
@@ -55,7 +54,7 @@ mod tests {
     use condition::ConditionBase::*;
     use condition::ConditionExpression::*;
     use condition::ConditionTree;
-    use table::Table;
+    use table::{TableObject, TablePartitionList};
 
     #[test]
     fn simple_delete() {
@@ -64,7 +63,7 @@ mod tests {
         assert_eq!(
             res.unwrap().1,
             DeleteStatement {
-                table: Table::from("users"),
+                table: TableObject::from("users"),
                 ..Default::default()
             }
         );
@@ -77,7 +76,7 @@ mod tests {
         assert_eq!(
             res.unwrap().1,
             DeleteStatement {
-                table: Table::from(("db1", "users")),
+                table: TableObject::from(("db1", "users")),
                 ..Default::default()
             }
         );
@@ -96,7 +95,30 @@ mod tests {
         assert_eq!(
             res.unwrap().1,
             DeleteStatement {
-                table: Table::from("users"),
+                table: TableObject::from("users"),
+                where_clause: expected_where_cond,
+                ..Default::default()
+            }
+        );
+    }
+
+    #[test]
+    fn delete_with_partition() {
+        let qstring = "DELETE FROM users PARTITION (u) WHERE id = 1;";
+        let res = deletion(qstring.as_bytes());
+        let expected_left = Base(Field(Column::from("id")));
+        let expected_where_cond = Some(ComparisonOp(ConditionTree {
+            left: Box::new(expected_left),
+            right: Box::new(Base(Literal(Literal::Integer(1)))),
+            operator: Operator::Equal,
+        }));
+        assert_eq!(
+            res.unwrap().1,
+            DeleteStatement {
+                table: TableObject {
+                    table: "users".into(),
+                    partitions: TablePartitionList(vec!["u".into()]),
+                },
                 where_clause: expected_where_cond,
                 ..Default::default()
             }
